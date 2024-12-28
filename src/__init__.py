250746d56b80b5441cacf89cd0ed4b30a4227582
Lorenzo Modica <lorenzomod@hotmail.it>
2024-12-28 14:09:47 +0100

    Update __init__.py
    To make the different classes importable from the package

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .dataManager import DataManager
+from .dataImportExport import DataExporter, DataImporter
+from .LogManager import LogManager
+from .StatisticalAnalyzer import StatisticalAnalyzer
